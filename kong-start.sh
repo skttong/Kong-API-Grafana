36fd7bcbe6c450d77931a1b2543ef2b964193407
skttong <sommanuttong@gmail.com>
2023-08-06 16:17:47 +0700

    Update kong-start.sh

--- kong-start.sh
+++ kong-start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-echo "Starting Graylog..."
-
-docker-compose up -d graylog elasticsearch mongo
-
-echo "Graylog running http://127.0.0.1:9000/"
-
 echo "Starting kong-database..."
-
-#docker volume rm $(docker volume ls -q)
 
 docker-compose up -d kong-database
 
@@ -30,13 +22,15 @@ echo "Starting kong..."
 docker-compose up -d kong
 
 echo "Kong admin running http://0.0.0.0:8001/"
-echo "Kong proxy running http://0.0.0.0:8000/"
+echo "Kong proxy running http://0.0.0.0/"
 
 echo "Starting konga..."
 
-docker-compose up -d konga
+docker-compose up -d mongo konga
 
 echo "Konga running http://0.0.0.0:1337/"
+
+echo "Starting grafana..."
 
 docker-compose up -d grafana
 
@@ -47,9 +41,3 @@ echo "Starting prometheus..."
 docker-compose up -d prometheus node_exporter
 
 echo "Prometheus running http://0.0.0.0:9090/"
-
-echo "Starting API & DB..."
-
-docker-compose up -d db api 
-
-echo "API on http://0.0.0.0:5000/"
